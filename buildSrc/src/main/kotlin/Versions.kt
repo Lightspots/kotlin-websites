c1000ae3ec9db1e19d338c52d133553b3ff969cd
Benjamin Leber <grisu118@grisu118.ch>
2020-04-19 15:42:10 +0200

    🔨 Extract versions to buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+  const val coroutines = "1.3.5"
+  const val reactKotlin = "16.13.0-pre.98-kotlin-1.3.72"
+  const val reactRouterKotlin = "4.3.1-pre.98-kotlin-1.3.72"
+  const val styledKotlin = "1.0.0-pre.98-kotlin-1.3.72"
+
+  const val react = "16.13.0"
+  const val reactRouter = "5.1.2"
+}
--- reitstall-goldberghof.de/build.gradle.kts
+++ reitstall-goldberghof.de/build.gradle.kts
@@ -6,18 +6,18 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
   implementation(kotlin("stdlib-js"))
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.3-1.3.70-eap-42")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:${Versions.coroutines}")
 
   //React, React DOM + Wrappers
-  implementation("org.jetbrains:kotlin-react:16.13.0-pre.92-kotlin-1.3.61")
-  implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.92-kotlin-1.3.61")
-  implementation("org.jetbrains:kotlin-react-router-dom:4.3.1-pre.92-kotlin-1.3.61")
-  implementation(npm("react", "16.13.0"))
-  implementation(npm("react-dom", "16.13.0"))
-  implementation(npm("react-router-dom", "5.1.2"))
+  implementation("org.jetbrains:kotlin-react:${Versions.reactKotlin}")
+  implementation("org.jetbrains:kotlin-react-dom:${Versions.reactKotlin}")
+  implementation("org.jetbrains:kotlin-react-router-dom:${Versions.reactRouterKotlin}")
+  implementation(npm("react", Versions.react))
+  implementation(npm("react-dom", Versions.react))
+  implementation(npm("react-router-dom", Versions.reactRouter))
 
   //Kotlin Styled
-  implementation("org.jetbrains:kotlin-styled:1.0.0-pre.92-kotlin-1.3.61")
+  implementation("org.jetbrains:kotlin-styled:${Versions.styledKotlin}")
   implementation(npm("styled-components"))
   implementation(npm("inline-style-prefixer"))
 }
